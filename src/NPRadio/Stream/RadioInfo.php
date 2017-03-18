f83b1150abcaaecc840efb4fc37a410974868994
Bernd Stellwag <burned@zerties.org>
2017-03-18 22:12:02 +0100

    rearrange radio info and add method to get data as array

--- src/NPRadio/Stream/RadioInfo.php
+++ src/NPRadio/Stream/RadioInfo.php
@@ -5,12 +5,17 @@ namespace NPRadio\Stream;
 class RadioInfo
 {
     /** @var string */
-    protected $track = null;
+    protected $radioName = null;
+
     /** @var string */
-    protected $artist = null;
+    protected $streamName = null;
+    /** @var string */
+    protected $homepageUrl = null;
 
     /** @var string */
     protected $show = null;
+    /** @var string */
+    protected $genre = null;
     /** @var string */
     protected $moderator = null;
     /** @var \DateTime */
@@ -19,36 +24,30 @@ class RadioInfo
     protected $showEndTime = null;
 
     /** @var string */
-    protected $radioName = null;
+    protected $track = null;
     /** @var string */
-    protected $homepageUrl = null;
+    protected $artist = null;
 
-    /** @var string */
-    protected $streamName = null;
-    /** @var string */
-    protected $genre = null;
+    public function getAsArray()
+    {
+        return [
+            'radio_name'  => $this->getRadioName(),
+            'stream_name' => $this->getStreamName(),
+            'homepage'    => $this->getHomepageUrl(),
+            'show'        => [
+                'name'       => $this->getShow(),
+                'genre'      => $this->getGenre(),
+                'moderator'  => $this->getModerator(),
+                'start_time' => $this->getShowStartTime(),
+                'end_time'   => $this->getShowEndTime()
+            ],
+            'track'       => $this->getTrack(),
+            'artist'      => $this->getArtist()
+        ];
+    }
 
     /**
      * @return string
-     */
-    public function getHomepageUrl(): string
-    {
-        return $this->homepageUrl;
-    }
-
-    /**
-     * @param string $homepageUrl
-     * @return RadioInfo
-     */
-    public function setHomepageUrl(string $homepageUrl): RadioInfo
-    {
-        $this->homepageUrl = $homepageUrl;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
      */
     public function getRadioName(): string
     {
@@ -56,126 +55,12 @@ class RadioInfo
     }
 
     /**
-     * @param mixed $radioName
+     * @param string $radioName
      * @return RadioInfo
      */
-    public function setRadioName($radioName): RadioInfo
+    public function setRadioName(string $radioName): RadioInfo
     {
         $this->radioName = $radioName;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getTrack(): string
-    {
-        return $this->track;
-    }
-
-    /**
-     * @param string $track
-     * @return RadioInfo
-     */
-    public function setTrack(string $track): RadioInfo
-    {
-        $this->track = $track;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getArtist(): string
-    {
-        return $this->artist;
-    }
-
-    /**
-     * @param string $artist
-     * @return RadioInfo
-     */
-    public function setArtist(string $artist): RadioInfo
-    {
-        $this->artist = $artist;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getShow(): string
-    {
-        return $this->show;
-    }
-
-    /**
-     * @param string $show
-     * @return RadioInfo
-     */
-    public function setShow(string $show): RadioInfo
-    {
-        $this->show = $show;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getModerator(): string
-    {
-        return $this->moderator;
-    }
-
-    /**
-     * @param string $moderator
-     * @return RadioInfo
-     */
-    public function setModerator(string $moderator): RadioInfo
-    {
-        $this->moderator = $moderator;
-
-        return $this;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getShowStartTime(): \DateTime
-    {
-        return $this->showStartTime;
-    }
-
-    /**
-     * @param \DateTime $showStartTime
-     * @return RadioInfo
-     */
-    public function setShowStartTime(\DateTime $showStartTime): RadioInfo
-    {
-        $this->showStartTime = $showStartTime;
-
-        return $this;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getShowEndTime(): \DateTime
-    {
-        return $this->showEndTime;
-    }
-
-    /**
-     * @param \DateTime $showEndTime
-     * @return RadioInfo
-     */
-    public function setShowEndTime(\DateTime $showEndTime): RadioInfo
-    {
-        $this->showEndTime = $showEndTime;
 
         return $this;
     }
@@ -202,7 +87,45 @@ class RadioInfo
     /**
      * @return string
      */
-    public function getGenre(): string
+    public function getHomepageUrl(): string
+    {
+        return $this->homepageUrl;
+    }
+
+    /**
+     * @param string $homepageUrl
+     * @return RadioInfo
+     */
+    public function setHomepageUrl(string $homepageUrl): RadioInfo
+    {
+        $this->homepageUrl = $homepageUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getShow()
+    {
+        return $this->show;
+    }
+
+    /**
+     * @param string $show
+     * @return RadioInfo
+     */
+    public function setShow(string $show): RadioInfo
+    {
+        $this->show = $show;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGenre()
     {
         return $this->genre;
     }
@@ -214,6 +137,101 @@ class RadioInfo
     public function setGenre(string $genre): RadioInfo
     {
         $this->genre = $genre;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getModerator()
+    {
+        return $this->moderator;
+    }
+
+    /**
+     * @param string $moderator
+     * @return RadioInfo
+     */
+    public function setModerator(string $moderator): RadioInfo
+    {
+        $this->moderator = $moderator;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getShowStartTime()
+    {
+        return $this->showStartTime;
+    }
+
+    /**
+     * @param \DateTime $showStartTime
+     * @return RadioInfo
+     */
+    public function setShowStartTime(\DateTime $showStartTime): RadioInfo
+    {
+        $this->showStartTime = $showStartTime;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getShowEndTime()
+    {
+        return $this->showEndTime;
+    }
+
+    /**
+     * @param \DateTime $showEndTime
+     * @return RadioInfo
+     */
+    public function setShowEndTime(\DateTime $showEndTime): RadioInfo
+    {
+        $this->showEndTime = $showEndTime;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTrack()
+    {
+        return $this->track;
+    }
+
+    /**
+     * @param string $track
+     * @return RadioInfo
+     */
+    public function setTrack(string $track): RadioInfo
+    {
+        $this->track = $track;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * @param string $artist
+     * @return RadioInfo
+     */
+    public function setArtist(string $artist): RadioInfo
+    {
+        $this->artist = $artist;
 
         return $this;
     }
