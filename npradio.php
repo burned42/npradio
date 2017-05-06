fd262c3585e666973fb5c95eb186ce08a820ba29
Bernd Stellwag <burned@zerties.org>
2017-05-06 17:26:03 +0200

    format info echoed by cli script

--- npradio.php
+++ npradio.php
@@ -6,40 +6,56 @@ require_once 'vendor/autoload.php';
 use NPRadio\Stream\MetalOnly;
 use NPRadio\Stream\RadioContainer;
 use NPRadio\DataFetcher\HttpDomFetcher;
-use NPRadio\Stream\RadioStream;
 use NPRadio\Stream\RauteMusik;
 use NPRadio\Stream\TechnoBase;
 
 
-$radioStreams = [
-    MetalOnly::class => [
-        MetalOnly::METAL_ONLY
-    ],
-    RauteMusik::class => [
-        RauteMusik::MAIN,
-        RauteMusik::CLUB
-    ],
-    TechnoBase::class => [
-        TechnoBase::TECHNOBASE,
-        TechnoBase::HOUSETIME
-    ]
-];
+try {
+    $radioContainer = new RadioContainer();
+    $domFetcher = new HttpDomFetcher();
+    $radioStreams = [
+        MetalOnly::class,
+        RauteMusik::class,
+        TechnoBase::class
+    ];
+    foreach ($radioStreams as $radioStream) {
+        $radioContainer->addRadio(new $radioStream($domFetcher));
+    }
 
-$domFetcher = new HttpDomFetcher();
-$radioContainer = new RadioContainer();
-/** @var RadioStream $radioStream */
-foreach ($radioStreams as $radioStream => $streams) {
-    /** @var RadioStream $stream */
-    $stream = new $radioStream($domFetcher);
-    $radioName = $stream->getRadioName();
-    $radioContainer->addRadio($stream);
+    foreach ($radioContainer->getRadioNames() as $radioName) {
+        echo $radioName . ":\n";
+        foreach ($radioContainer->getStreamNames($radioName) as $streamName) {
+            echo "    " . $streamName . ":\n";
+            $streamInfo = $radioContainer->getInfo($radioName, $streamName);
 
-    foreach ($streams as $streamName) {
-        echo $streamName . "\n";
-        try {
-            var_dump($radioContainer->getInfo($radioName, $streamName));
-        } catch (\Exception $e) {
-            echo 'could not get info from ' . $streamName . ': ' . $e->getMessage() . "\n";
+            if (!is_null($streamInfo->getShow())) {
+                echo "        Show:      " . $streamInfo->getShow() . "\n";
+            }
+            if (!is_null($streamInfo->getGenre())) {
+                echo "        Genre:     " . $streamInfo->getGenre() . "\n";
+            }
+            if (!is_null($streamInfo->getModerator())) {
+                echo "        Moderator: " . $streamInfo->getModerator() . "\n";
+            }
+            if (
+                $streamInfo->getShowStartTime() instanceof DateTime
+                && $streamInfo->getShowEndTime() instanceof DateTime
+            ) {
+                echo "        Showtime:  " . $streamInfo->getShowStartTime()->format('H:i')
+                    . ' - ' . $streamInfo->getShowEndTime()->format('H:i') . "\n";
+            }
+            echo "        Track:     ";
+            if (!is_null($streamInfo->getTrack())) {
+                echo $streamInfo->getTrack();
+                if (!is_null($streamInfo->getArtist())) {
+                    echo ' - ' . $streamInfo->getArtist();
+                }
+            } else {
+                echo 'n/a';
+            }
+            echo "\n";
         }
     }
+} catch (Exception $e) {
+    echo 'got exception: ' . $e->getMessage();
 }
