feedd126957ddbe256615789a3a9847f468cd979
Bernd Stellwag <burned@zerties.org>
2017-08-25 18:58:32 +0200

    refactor api to separate endpoint and to separate file

---
+++ src/NPRadio/Controller/ApiController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace NPRadio\Controller;
+
+use NPRadio\DataFetcher\HttpDomFetcher;
+use NPRadio\Stream\MetalOnly;
+use NPRadio\Stream\RadioContainer;
+use NPRadio\Stream\RauteMusik;
+use NPRadio\Stream\TechnoBase;
+use Silex\Api\ControllerProviderInterface;
+use Silex\Application;
+use Silex\ControllerCollection;
+
+class ApiController implements ControllerProviderInterface
+{
+    protected $radioContainer;
+
+    public function __construct()
+    {
+        $this->radioContainer = new RadioContainer();
+        $domFetcher = new HttpDomFetcher();
+        $radioStreams = [
+            MetalOnly::class,
+            RauteMusik::class,
+            TechnoBase::class
+        ];
+
+        foreach ($radioStreams as $radioStream) {
+            $this->radioContainer->addRadio(new $radioStream($domFetcher));
+        }
+    }
+
+    /**
+     * Returns routes to connect to the given application.
+     *
+     * @param Application $app An Application instance
+     *
+     * @return ControllerCollection A ControllerCollection instance
+     */
+    public function connect(Application $app)
+    {
+        $radioContainer = $this->radioContainer;
+        $controller = $app['controllers_factory'];
+
+        $controller->get(
+            '/radios',
+            function (Application $app) use ($radioContainer) {
+                return $app->json($radioContainer->getRadioNames());
+            }
+        );
+        $controller->get(
+            '/radios/{radioName}/streams',
+            function (Application $app, string $radioName) use ($radioContainer) {
+                return $app->json(
+                    $this->radioContainer->getStreamNames($radioName)
+                );
+            }
+        );
+        $controller->get(
+            'radios/{radioName}/streams/{streamName}',
+            function (Application $app, string $radioName, string $streamName) use ($radioContainer) {
+                return $app->json(
+                    $radioContainer->getInfo($radioName, $streamName)->getAsArray()
+                );
+            }
+        );
+
+        return $controller;
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,47 +1,14 @@
 <?php
 
-use NPRadio\DataFetcher\HttpDomFetcher;
-use NPRadio\Stream\MetalOnly;
-use NPRadio\Stream\RadioContainer;
-use NPRadio\Stream\RauteMusik;
-use NPRadio\Stream\TechnoBase;
 use Silex\Application;
 
 require_once '../vendor/autoload.php';
 
-$app = new Silex\Application();
+$app = new Application();
+
+// TODO disable debugging eventually
 $app['debug'] = true;
 
-try {
-    $radioContainer = new RadioContainer();
-    $domFetcher = new HttpDomFetcher();
-    $radioStreams = [
-        MetalOnly::class,
-        RauteMusik::class,
-        TechnoBase::class
-    ];
-    foreach ($radioStreams as $radioStream) {
-        $radioContainer->addRadio(new $radioStream($domFetcher));
-    }
-} catch (\Exception $e) {
-    $app->abort(500, $e->getMessage());
-}
-
-$app->get('/radios', function (Application $app) use ($radioContainer) {
-    return $app->json($radioContainer->getRadioNames());
-});
-$app->get('/radios/{radioName}/streams', function (Application $app, string $radioName) use ($radioContainer) {
-    return $app->json(
-        $radioContainer->getStreamNames($radioName)
-    );
-});
-$app->get(
-    'radios/{radioName}/streams/{streamName}',
-    function (Application $app, string $radioName, string $streamName) use ($radioContainer) {
-        return $app->json(
-            $radioContainer->getInfo($radioName, $streamName)->getAsArray()
-        );
-    }
-);
+$app->mount('/api', new \NPRadio\Controller\ApiController());
 
 $app->run();
