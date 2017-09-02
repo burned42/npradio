fc31aa851e26601fc8603df23a76e3568968ef63
Bernd Stellwag <burned@zerties.org>
2017-09-02 16:47:08 +0200

    add stream url to stream info

--- src/NPRadio/Stream/MetalOnly.php
+++ src/NPRadio/Stream/MetalOnly.php
@@ -8,7 +8,7 @@ class MetalOnly extends RadioStream
     const URL = 'https://www.metal-only.de';
     # use page 'Impressum' because there is only text and the page should load quicker
     const URL_INFO_PATH = '/impressum.html';
-    const STREAM_URL = 'http://server1.blitz-stream.de:4400/';
+    const STREAM_URL = 'http://server1.blitz-stream.de:4400/;';
 
     const METAL_ONLY = 'MetalOnly';
 
