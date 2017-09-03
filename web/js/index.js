7e76af1abc5ced79284f3eaa2e70389a8f100c4d
Bernd Stellwag <burned@zerties.org>
2017-09-03 13:58:58 +0200

    refactor the way the html content is composed

--- web/js/index.js
+++ web/js/index.js
@@ -26,78 +26,121 @@ function RadioStream(radioName, streamName) {
 
     this.domElement = document.createElement('div');
     this.domElement.className = 'card invisible';
+    this.domElementInitialized = false;
+    let cardBody;
+    let cardFooter;
+    let cardFooterAppended = false;
+
     document.getElementById('stream_infos').appendChild(this.domElement);
 
     let self = this;
 
-
     this.update = async function () {
         if (self.requestRunning === false) {
-            let div = self.domElement;
             self.requestRunning = true;
             let result = await get(self.streamInfoUrl);
-            // TODO format div once
-            // TODO then only replace the content on update
-            div.innerHTML = self.formatStreamInfo(result);
-            div.className = 'card';
+            self.updateStreamInfoDom(result);
             self.requestRunning = false;
         }
     };
 
-    this.formatStreamInfo = function (streamInfo) {
-        let html = '';
-
-        html += '<h5 class="card-header text-nowrap d-flex justify-content-between">' +
-            '<a href="' + streamInfo.homepage + '" class="align-self-center"> ' + streamInfo.radio_name + ': ' + streamInfo.stream_name + '</a>' +
-            '<button type="button" class="btn btn-secondary" name="play_stream" onclick="playStream(this, \'' + streamInfo.stream_url + '\')">&#x25b6;</button>' +
-            '</h5>';
-
+    this.updateStreamInfoDom = function (streamInfo) {
         if (streamInfo.artist === null) {
             streamInfo.artist = 'n/a';
         }
         if (streamInfo.track === null) {
             streamInfo.track = 'n/a';
         }
-        html += '<div class="card-body">' +
-            '<strong>' + streamInfo.artist + '</strong>' +
-            '<span class="text-muted px-2 px-sm-2 px-md-2 px-lg-2 px-xl-2">-</span>' +
-            '<strong>' + streamInfo.track + '</strong>' +
-            '</div>';
 
+        let bodyContent = '<strong>' + streamInfo.artist + '</strong>' +
+            '<span class="text-muted px-2 px-sm-2 px-md-2 px-lg-2 px-xl-2">-</span>' +
+            '<strong>' + streamInfo.track + '</strong>';
+
+        let footerContent = false;
         if (
             streamInfo.show.name !== null
             && streamInfo.show.moderator !== null
         ) {
-            html += '<div class="card-footer alert alert-danger mb-0 mb-lg-0 mb-md-0 mb-sm-0 mb-xl-0">';
 
-            html += '<strong>' + streamInfo.show.name + '</strong>';
+            footerContent = '<strong>' + streamInfo.show.name + '</strong>';
             if (streamInfo.show.genre !== null) {
-                html += ' (' + streamInfo.show.genre + ')';
+                footerContent += ' (' + streamInfo.show.genre + ')';
             }
 
-            html += '<hr>mit <strong>' + streamInfo.show.moderator + '</strong>';
+            footerContent += '<hr>mit <strong>' + streamInfo.show.moderator + '</strong>';
             if (streamInfo.show.start_time !== null && streamInfo.show.end_time !== null) {
-                html += ' (' + streamInfo.show.start_time + ' - ' + streamInfo.show.end_time + ')';
+                footerContent += ' (' + streamInfo.show.start_time + ' - ' + streamInfo.show.end_time + ')';
             }
-
-            html += '</div>';
         }
 
-        return html;
+        if (self.domElementInitialized === false) {
+            // add header
+            let header = document.createElement('h5');
+            header.className = 'card-header text-nowrap d-flex justify-content-between';
+
+            let headerLink = document.createElement('a');
+            headerLink.className = 'align-self-center';
+            headerLink.setAttribute('href', streamInfo.homepage);
+            headerLink.setAttribute('target', '_blank');
+            headerLink.innerHTML = streamInfo.radio_name + ': ' + streamInfo.stream_name;
+            header.appendChild(headerLink);
+
+            let headerButton = document.createElement('button');
+            headerButton.className = 'btn btn-secondary';
+            headerButton.name = 'play_stream';
+            headerButton.onclick = function () {
+                playStream(this, streamInfo.stream_url);
+            };
+            headerButton.innerHTML = '&#x25b6';
+            header.appendChild(headerButton);
+
+            self.domElement.appendChild(header);
+
+
+            // add body
+            cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+            self.domElement.appendChild(cardBody);
+
+            // add footer
+            cardFooter = document.createElement('div');
+            cardFooter.className = 'card-footer alert alert-danger mb-0';
+
+            self.domElement.className = 'card';
+            self.domElementInitialized = true;
+        }
+
+        cardBody.innerHTML = bodyContent;
+
+        if (footerContent) {
+            cardFooter.innerHTML = footerContent;
+            if (cardFooterAppended === false) {
+                self.domElement.appendChild(cardFooter);
+                cardFooterAppended = true;
+            }
+        } else if (cardFooterAppended) {
+            self.domElement.removeChild(cardFooter);
+            cardFooterAppended = false;
+        }
     };
 }
 
-function playStream(e, streamUrl) {
+function initializePlayButtonsToPaused() {
     let playButtons = document.getElementsByName('play_stream');
     for (let i = 0; i < playButtons.length; i++) {
         playButtons[i].className = 'btn btn-secondary';
         playButtons[i].innerHTML = '&#x25b6;';
     }
+}
+
+function playStream(e, streamUrl) {
+    initializePlayButtonsToPaused();
 
     let streamPlayer = document.getElementById('stream_player');
+    let wasPaused = streamPlayer.paused;
     streamPlayer.pause();
 
-    if (streamPlayer.getAttribute('src') !== streamUrl) {
+    if (streamPlayer.getAttribute('src') !== streamUrl || wasPaused === true) {
         streamPlayer.setAttribute('src', streamUrl);
         streamPlayer.play();
 
