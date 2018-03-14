239bdb8b38fda57a74d3a9f11d061452e310dc81
Bernd Stellwag <burned@zerties.org>
2018-03-14 21:34:13 +0100

    fetch showtime for metal only

--- src/NPRadio/Stream/MetalOnly.php
+++ src/NPRadio/Stream/MetalOnly.php
@@ -9,7 +9,7 @@ class MetalOnly extends RadioStream
     const RADIO_NAME = 'MetalOnly';
     const URL = 'https://www.metal-only.de';
     // use page 'Impressum' because there is only text and the page should load quicker
-    const URL_INFO_PATH = '/impressum.html';
+    const URL_INFO_PATH = '/sendeplan.html';
     const STREAM_URL = 'http://server1.blitz-stream.de:4400/;';
 
     const METAL_ONLY = 'MetalOnly';
@@ -41,7 +41,7 @@ class MetalOnly extends RadioStream
         $streamInfo = $this->getStreamInfo($streamName);
 
         try {
-            $dom = $this->domFetcher->getHtmlDom(self::URL);
+            $dom = $this->domFetcher->getHtmlDom(self::URL.self::URL_INFO_PATH);
         } catch (\Exception $e) {
             throw new \RuntimeException('could not get html dom: '.$e->getMessage());
         }
@@ -88,6 +88,53 @@ class MetalOnly extends RadioStream
                 $streamInfo->setArtist(trim($matches[1]));
                 $streamInfo->setTrack(trim($matches[2]));
             }
+        }
+
+        // fetch showtime
+        $dayOfWeek = date('N');
+        $nodeList = $xpath->query("(.//div[@class='sendeplan']//div[@class='day']//ul[@class='list'])[".$dayOfWeek.']//li[position()>2]');
+
+        $lastModerator = null;
+        $foundAt = false;
+        $startTime = null;
+        $endTime = null;
+        for ($i = 0; $i < $nodeList->length; ++$i) {
+            $item = $nodeList->item($i)->firstChild;
+            $moderator = $item->nodeValue;
+
+            // if moderator changed since last loop run
+            if ($lastModerator !== $moderator) {
+                // and if we didn't find the on air mod until now
+                if (false === $foundAt) {
+                    // set new value for start time
+                    $startTime = new \DateTime(($i + 14).':00');
+                }
+                // or we did already find the on air mod and can stop here
+                else {
+                    break;
+                }
+            }
+
+            if ('nowonair' === trim($item->getAttribute('class'))) {
+                $foundAt = $i;
+            }
+
+            // if we have found the on air mod and are still running the for loop
+            if (false !== $foundAt) {
+                // then set the end time to the start time of this column (of the html) + 1
+                $endTime = new \DateTime(($i + 14 + 1).':00');
+            }
+
+            $lastModerator = $moderator;
+        }
+
+        if (
+            false !== $foundAt
+            && $startTime instanceof \DateTime
+            && $endTime instanceof \DateTime
+        ) {
+            $streamInfo->setShowStartTime($startTime);
+            $streamInfo->setShowEndTime($endTime);
         }
 
         return $streamInfo;
