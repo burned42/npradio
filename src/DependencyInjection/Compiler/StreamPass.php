28256670986cda44351186735093f421ff2413a2
Bernd Stellwag <burned@zerties.org>
2018-10-19 20:18:52 +0200

    use dependency injection instead of listing all streams manually

---
+++ src/DependencyInjection/Compiler/StreamPass.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DependencyInjection\Compiler;
+
+use App\Controller\ApiController;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class StreamPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->has(ApiController::class)) {
+            return;
+        }
+
+        $definition = $container->findDefinition(ApiController::class);
+
+        $streamServices = $container->findTaggedServiceIds('app.stream');
+
+        foreach ($streamServices as $id => $service) {
+            $definition->addMethodCall('addRadio', [$id]);
+        }
+    }
+}
