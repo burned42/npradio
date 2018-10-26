a4630e3479e454c5d4ccb8366f0e824e56497b04
Bernd Stellwag <burned@zerties.org>
2018-10-27 01:16:33 +0200

    add test for stream compiler pass class

---
+++ tests/unit/DependencyInjection/Compiler/StreamPassCest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\DependencyInjection\Compiler;
+
+use App\Controller\ApiController;
+use App\DependencyInjection\Compiler\StreamPass;
+use App\Stream\AbstractRadioStream;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use UnitTester;
+
+class StreamPassCest
+{
+    /** @var StreamPass */
+    private $streamPass;
+
+    public function _before()
+    {
+        $this->streamPass = new StreamPass();
+    }
+
+    public function canInstantiate(UnitTester $I)
+    {
+        $I->assertInstanceOf(StreamPass::class, $this->streamPass);
+    }
+
+    public function testCanProcessEmptyContainer(UnitTester $I)
+    {
+        $container = new ContainerBuilder();
+
+        $this->streamPass->process($container);
+
+        $I->assertTrue(true, 'no error on processing');
+    }
+
+    public function testCanProcessFilledContainer(UnitTester $I)
+    {
+        $container = new ContainerBuilder();
+        $container->setDefinition(ApiController::class, new Definition(ApiController::class));
+        $container->register(AbstractRadioStream::class)
+            ->addTag('app.stream');
+
+        $this->streamPass->process($container);
+
+        $I->assertTrue(true, 'no error on processing');
+    }
+}
