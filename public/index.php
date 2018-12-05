25633bbb77cf80946338357dca023469ed9cd38d
Bernd Stellwag <burned@zerties.org>
2018-12-05 20:14:38 +0100

    update to symfony 4.2

--- public/index.php
+++ public/index.php
@@ -5,27 +5,11 @@ declare(strict_types=1);
 use App\CacheKernel;
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__.'/../vendor/autoload.php';
+require dirname(__DIR__).'/src/.bootstrap.php';
 
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException(
-            'APP_ENV environment variable is not defined.'
-            .' You need to define environment variables for configuration'
-            .' or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.'
-        );
-    }
-    (new Dotenv())->load(__DIR__.'/../.env');
-}
-
-$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
-
-if ($debug) {
+if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
     Debug::enable();
@@ -42,7 +26,7 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
     Request::setTrustedHosts(explode(',', $trustedHosts));
 }
 
-$kernel = new Kernel($env, $debug);
+$kernel = new Kernel($_SERVER['APP_ENV'], $_SERVER['APP_DEBUG']);
 
 // Wrap the default Kernel with the CacheKernel one in 'prod' environment
 if ('prod' === $env) {
