1f235c6187b2dacd0f63efba12b53f21d963eaaa
Bernd Stellwag <burned@zerties.org>
2020-10-13 22:43:45 +0200

    adjust preload file to only load the file in prod env (even if it exists)

--- config/preload.php
+++ config/preload.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
-if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
+require_once 'vendor/symfony/dotenv/Dotenv.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if (
+    'prod' === $_SERVER['APP_ENV']
+    && file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')
+) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
