57c29dcb601ac8a48faf47981b77443c0ab7b197
Bernd Stellwag <burned@zerties.org>
2020-11-02 21:01:25 +0100

    remove unused properties from AbstractRadioStream

--- src/Stream/AbstractRadioStream.php
+++ src/Stream/AbstractRadioStream.php
@@ -5,19 +5,10 @@ declare(strict_types=1);
 namespace App\Stream;
 
 use App\DataFetcher\DomFetcherInterface;
-use DateTimeInterface;
 
 abstract class AbstractRadioStream
 {
     private DomFetcherInterface $domFetcher;
-
-    protected ?string $track = null;
-    protected ?string $artist = null;
-    protected ?string $show = null;
-    protected ?string $genre = null;
-    protected ?string $moderator = null;
-    protected ?DateTimeInterface $showStartTime = null;
-    protected ?DateTimeInterface $showEndTime = null;
 
     public function __construct(DomFetcherInterface $domFetcher)
     {
