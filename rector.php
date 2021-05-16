c091f633190e6ff3329e9e22f56dfbba21aaa173
Bernd Stellwag <burned@zerties.org>
2021-05-16 21:50:20 +0200

    composer update
    Lock file operations: 5 installs, 36 updates, 2 removals
      - Removing nette/robot-loader (v3.4.0)
      - Removing rector/rector-installer (0.10.1)
      - Upgrading doctrine/annotations (1.12.1 => 1.13.1)
      - Locking ergebnis/json-printer (3.1.1)
      - Locking idiosyncratic/editorconfig (0.1.1)
      - Upgrading psalm/plugin-symfony (v2.2.4 => v2.3.0)
      - Locking rector/extension-installer (0.10.2)
      - Upgrading rector/rector (0.10.19 => 0.10.22)
      - Upgrading rector/rector-nette (0.10.9 => 0.10.10)
      - Locking rector/rector-nette-to-symfony (0.10.0)
      - Upgrading sentry/sentry-symfony (4.1.0 => 4.1.1)
      - Locking shanethehat/pretty-xml (1.0.2)
      - Upgrading softcreatr/jsonpath (0.7.3 => 0.7.4)
      - Upgrading symfony/cache (v5.2.7 => v5.2.8)
      - Upgrading symfony/config (v5.2.7 => v5.2.8)
      - Upgrading symfony/console (v5.2.7 => v5.2.8)
      - Upgrading symfony/dependency-injection (v5.2.7 => v5.2.8)
      - Upgrading symfony/error-handler (v5.2.7 => v5.2.8)
      - Upgrading symfony/finder (v5.2.4 => v5.2.8)
      - Upgrading symfony/http-client (v5.2.7 => v5.2.8)
      - Upgrading symfony/http-foundation (v5.2.7 => v5.2.8)
      - Upgrading symfony/http-kernel (v5.2.7 => v5.2.8)
      - Upgrading symfony/phpunit-bridge (v5.2.7 => v5.2.8)
      - Upgrading symfony/security-core (v5.2.7 => v5.2.8)
      - Upgrading symfony/string (v5.2.6 => v5.2.8)
      - Upgrading symfony/twig-bridge (v5.2.7 => v5.2.8)
      - Upgrading symfony/var-dumper (v5.2.7 => v5.2.8)
      - Upgrading symfony/var-exporter (v5.2.7 => v5.2.8)
      - Upgrading symplify/astral (v9.3.1 => v9.3.4)
      - Upgrading symplify/autowire-array-parameter (v9.3.1 => v9.3.11)
      - Upgrading symplify/composer-json-manipulator (v9.3.1 => v9.3.11)
      - Upgrading symplify/console-color-diff (v9.3.1 => v9.3.11)
      - Upgrading symplify/console-package-builder (v9.3.1 => v9.3.11)
      - Upgrading symplify/easy-testing (v9.3.1 => v9.3.11)
      - Upgrading symplify/package-builder (v9.3.1 => v9.3.11)
      - Upgrading symplify/rule-doc-generator-contracts (v9.3.1 => v9.3.11)
      - Upgrading symplify/set-config-resolver (v9.3.1 => v9.3.11)
      - Upgrading symplify/simple-php-doc-parser (v9.3.1 => v9.3.11)
      - Upgrading symplify/skipper (v9.3.1 => v9.3.11)
      - Upgrading symplify/smart-file-system (v9.3.1 => v9.3.11)
      - Upgrading symplify/symfony-php-config (v9.3.0 => v9.3.10)
      - Upgrading symplify/symplify-kernel (v9.3.1 => v9.3.11)
      - Upgrading twig/twig (v3.3.0 => v3.3.2)

--- rector.php
+++ rector.php
@@ -2,23 +2,20 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
-    $parameters = $containerConfigurator->parameters();
+    // $parameters = $containerConfigurator->parameters();
 
     // Define what rule sets will be applied
-    $parameters->set(Option::SETS, [
-        SetList::CODE_QUALITY_STRICT,
-        SetList::PHP_80,
-        SetList::PSR_4,
-        SymfonySetList::SYMFONY_52,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-    ]);
+    $containerConfigurator->import(SetList::CODE_QUALITY_STRICT);
+    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(SetList::PSR_4);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_52);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
 
     // get services (needed for register a single rule)
     // $services = $containerConfigurator->services();
