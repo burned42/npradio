2bbef882efcd4963e40caf04e029784819fd5a2b
Bernd Stellwag <burned@zerties.org>
2021-06-03 12:35:56 +0200

    update symfony flex recipes

--- config/preload.php
+++ config/preload.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-require_once 'vendor/symfony/dotenv/Dotenv.php';
-
-use Symfony\Component\Dotenv\Dotenv;
-
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-
-if (
-    'prod' === $_SERVER['APP_ENV']
-    && file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')
-) {
+if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
