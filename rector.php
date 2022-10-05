9e53c52654ccd12ebf5762d1b80d5be7810a4160
Bernd Stellwag <burned@zerties.org>
2022-10-05 18:11:39 +0200

    add some symfony set lists to rector and sort the entries

--- rector.php
+++ rector.php
@@ -13,10 +13,16 @@ return static function (RectorConfig $rectorConfig): void {
     // $parameters = $rectorConfig->parameters();
 
     // Define what rule sets will be applied
-    $rectorConfig->import(SetList::PSR_4);
     $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
-    $rectorConfig->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
+    $rectorConfig->import(SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION);
+    $rectorConfig->import(SetList::PHP_81);
+    $rectorConfig->import(SetList::PSR_4);
     $rectorConfig->import(SymfonyLevelSetList::UP_TO_SYMFONY_60);
+    $rectorConfig->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
+    $rectorConfig->import(SymfonySetList::SYMFONY_60);
+    $rectorConfig->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    $rectorConfig->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
+    $rectorConfig->import(SymfonySetList::SYMFONY_STRICT);
 
     // get services (needed for register a single rule)
     // $services = $rectorConfig->services();
