f0badb555db8c1d9e72310d49fb8fc4aa255629b
Bernd Stellwag <burned@zerties.org>
2022-11-12 11:21:38 +0100

    rector: remove duplicate imports included in other set lists, add twig set list

--- rector.php
+++ rector.php
@@ -7,6 +7,7 @@ use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonyLevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
+use Rector\Symfony\Set\TwigLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     // get parameters
@@ -15,14 +16,12 @@ return static function (RectorConfig $rectorConfig): void {
     // Define what rule sets will be applied
     $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
     $rectorConfig->import(SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION);
-    $rectorConfig->import(SetList::PHP_81);
     $rectorConfig->import(SetList::PSR_4);
     $rectorConfig->import(SymfonyLevelSetList::UP_TO_SYMFONY_60);
-    $rectorConfig->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $rectorConfig->import(SymfonySetList::SYMFONY_60);
     $rectorConfig->import(SymfonySetList::SYMFONY_CODE_QUALITY);
     $rectorConfig->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
     $rectorConfig->import(SymfonySetList::SYMFONY_STRICT);
+    $rectorConfig->import(TwigLevelSetList::UP_TO_TWIG_240);
 
     // get services (needed for register a single rule)
     // $services = $rectorConfig->services();
