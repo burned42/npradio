b2b5e51da6a2d5771b47fec9ea0e5480a94092f6
Bernd Stellwag <burned@zerties.org>
2022-12-03 21:11:24 +0100

    composer update
    Lock file operations: 0 installs, 39 updates, 0 removals
      - Upgrading codeception/codeception (5.0.4 => 5.0.5)
      - Upgrading composer/pcre (3.0.2 => 3.1.0)
      - Upgrading nikic/php-parser (v4.15.1 => v4.15.2)
      - Upgrading phpunit/php-code-coverage (9.2.18 => 9.2.19)
      - Upgrading rector/rector (0.14.7 => 0.14.8)
      - Upgrading sentry/sentry (3.11.0 => 3.12.0)
      - Upgrading sentry/sentry-symfony (4.4.0 => 4.5.0)
      - Upgrading symfony/browser-kit (v6.1.3 => v6.2.0)
      - Upgrading symfony/cache (v6.1.7 => v6.2.0)
      - Upgrading symfony/config (v6.1.3 => v6.2.0)
      - Upgrading symfony/console (v6.1.7 => v6.2.0)
      - Upgrading symfony/css-selector (v6.1.3 => v6.2.0)
      - Upgrading symfony/dependency-injection (v6.1.5 => v6.2.0)
      - Upgrading symfony/dom-crawler (v6.1.7 => v6.2.0)
      - Upgrading symfony/dotenv (v6.1.0 => v6.2.0)
      - Upgrading symfony/error-handler (v6.1.7 => v6.2.0)
      - Upgrading symfony/event-dispatcher (v6.1.0 => v6.2.0)
      - Upgrading symfony/filesystem (v6.1.5 => v6.2.0)
      - Upgrading symfony/finder (v6.1.3 => v6.2.0)
      - Upgrading symfony/framework-bundle (v6.1.7 => v6.2.0)
      - Upgrading symfony/http-client (v6.1.7 => v6.2.0)
      - Upgrading symfony/http-foundation (v6.1.7 => v6.2.0)
      - Upgrading symfony/http-kernel (v6.1.7 => v6.2.0)
      - Upgrading symfony/options-resolver (v6.1.0 => v6.2.0)
      - Upgrading symfony/password-hasher (v6.1.3 => v6.2.0)
      - Upgrading symfony/phpunit-bridge (v6.1.6 => v6.2.0)
      - Upgrading symfony/process (v6.1.3 => v6.2.0)
      - Upgrading symfony/psr-http-message-bridge (v2.1.3 => v2.1.4)
      - Upgrading symfony/routing (v6.1.7 => v6.2.0)
      - Upgrading symfony/runtime (v6.1.3 => v6.2.0)
      - Upgrading symfony/security-core (v6.1.7 => v6.2.0)
      - Upgrading symfony/stopwatch (v6.1.5 => v6.2.0)
      - Upgrading symfony/string (v6.1.7 => v6.2.0)
      - Upgrading symfony/twig-bridge (v6.1.7 => v6.2.0)
      - Upgrading symfony/twig-bundle (v6.1.1 => v6.2.0)
      - Upgrading symfony/var-dumper (v6.1.6 => v6.2.0)
      - Upgrading symfony/var-exporter (v6.1.3 => v6.2.0)
      - Upgrading symfony/yaml (v6.1.6 => v6.2.0)
      - Upgrading twbs/bootstrap (v5.2.2 => v5.2.3)
      - Removing sensio/framework-extra-bundle (v6.2.9)

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
