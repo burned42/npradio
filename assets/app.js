bc10039b90d00e70212d065647ec092b2cfd27db
Bernd Stellwag <burned@zerties.org>
2023-12-11 20:25:04 +0100

    fix css file order

--- assets/app.js
+++ assets/app.js
@@ -4,8 +4,8 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.css'
 
 import RadioStream from "./radio_stream.js";
 import {Sortable} from "./vendor/sortablejs/sortablejs.index.js";
