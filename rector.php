6cc346330b1e0d43216ac52279966e5b137021b1
Bernd Stellwag <burned@zerties.org>
2024-02-10 15:36:27 +0100

    update rector config for version 1.0

--- rector.php
+++ rector.php
@@ -3,33 +3,25 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\Symfony\Set\TwigSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    // get parameters
-    // $parameters = $rectorConfig->parameters();
-
-    // Define what rule sets will be applied
-    $rectorConfig->import(LevelSetList::UP_TO_PHP_83);
-    $rectorConfig->import(SetList::EARLY_RETURN);
-    $rectorConfig->import(SetList::INSTANCEOF);
-    $rectorConfig->import(SetList::PRIVATIZATION);
-    $rectorConfig->import(SetList::STRICT_BOOLEANS);
-    $rectorConfig->import(SetList::TYPE_DECLARATION);
-    $rectorConfig->import(SymfonySetList::CONFIGS);
-    $rectorConfig->import(SymfonySetList::SYMFONY_64);
-    $rectorConfig->import(SymfonySetList::SYMFONY_CODE_QUALITY);
-    $rectorConfig->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
-    $rectorConfig->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $rectorConfig->import(TwigSetList::TWIG_240);
-    $rectorConfig->import(TwigSetList::TWIG_UNDERSCORE_TO_NAMESPACE);
-
-    // get services (needed for register a single rule)
-    // $services = $rectorConfig->services();
-
-    // register a single rule
-    // $services->set(TypedPropertyRector::class);
-};
+return RectorConfig::configure()
+    ->withPhpSets()
+    ->withImportNames()
+    ->withPreparedSets(
+        typeDeclarations: true,
+        privatization: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true
+    )
+    ->withAttributesSets(symfony: true)
+    ->withSets([
+        SymfonySetList::CONFIGS,
+        SymfonySetList::SYMFONY_64,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+        TwigSetList::TWIG_240,
+        TwigSetList::TWIG_UNDERSCORE_TO_NAMESPACE,
+    ]);
