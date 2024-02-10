0026895193203280bb582b43e0f67584a4a3c6da
Bernd Stellwag <burned@zerties.org>
2024-02-10 15:18:03 +0100

    replace deprecated class usage

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\Cache;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Traversable;
 
 use function Sentry\captureException;
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,7 +6,7 @@ namespace App\Controller;
 
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 final class HomepageController extends AbstractController
 {
