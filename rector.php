0c80a19fb4138733691e8913d4c5f9068bab4087
Bernd Stellwag <burned@zerties.org>
2025-06-18 21:17:25 +0200

    resolve rector deprecations

--- rector.php
+++ rector.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Set\SymfonySetList;
-use Rector\Symfony\Set\TwigSetList;
 
 return RectorConfig::configure()
     ->withPhpSets()
@@ -17,11 +15,7 @@ return RectorConfig::configure()
         strictBooleans: true,
     )
     ->withAttributesSets(symfony: true)
-    ->withSets([
-        SymfonySetList::CONFIGS,
-        SymfonySetList::SYMFONY_73,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        TwigSetList::TWIG_24,
-        TwigSetList::TWIG_UNDERSCORE_TO_NAMESPACE,
-    ]);
+    ->withComposerBased(
+        twig: true,
+        symfony: true,
+    );
