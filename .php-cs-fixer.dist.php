1187bfbf5844033234fcbb40f80d932a10b5d460
Bernd Stellwag <burned@zerties.org>
2025-10-05 17:35:15 +0200

    let php-cs-fixer run in parallel mode

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
-$finder = (new PhpCsFixer\Finder())
+$finder = new PhpCsFixer\Finder()
     ->in(__DIR__)
     ->exclude(['tests/Support/_generated', 'var/'])
 ;
 
-return (new PhpCsFixer\Config())
+return new PhpCsFixer\Config()
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
         'declare_strict_types' => true,
